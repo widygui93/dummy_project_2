a848bf802c80de5dcb9cf03cadaeec0df48c43cc
widygui93 <guilias.widy@gmail.com>
2020-02-26 21:02:07 +0700

    ubah jadi pake form dengan method post jadi pas direct ke order.php bisa sekalian kirim data nama,harga,tipe,isExtraSeafood dan isExtraHam tanpa kelihatan di url

--- App/Core/Order.php
+++ App/Core/Order.php
@@ -5,13 +5,31 @@ require_once '../init.php';
 use App\Menu\ChineseMainCourse as OrderChinese;
 use App\Menu\WesternMainCourse as OrderWestern;
 
-$tipeOrder = $_GET["q"];
+// $tipeOrder = $_GET["q"];
 
-if($tipeOrder == "chinese"){
-	$order = new OrderChinese("Kwetiau Goreng",35000,"y");
+// if($tipeOrder == "chinese"){
+// 	$order = new OrderChinese("Kwetiau Goreng",35000,"y");
+// 	echo $order->order();
+// } elseif($tipeOrder == "western") {
+// 	$order = new OrderWestern("Hamburger Deluxe",35000,"n");
+// 	echo $order->order();
+// } else {
+// 	echo "tipe menu tidak ditemukan";
+// }
+
+$type = $_POST['tipe'];
+$name = $_POST['nama'];
+$cost = $_POST['harga'];
+
+
+
+if($type == "chinese"){
+	$isExtraSeafood = $_POST['isExtraSeafood'];
+	$order = new OrderChinese($name,$cost,$isExtraSeafood);
 	echo $order->order();
-} elseif($tipeOrder == "western") {
-	$order = new OrderWestern("Hamburger Deluxe",35000,"n");
+} elseif($type == "western"){
+	$isExtraHam = $_POST['isExtraHam'];
+	$order = new OrderWestern($name,$cost,$isExtraHam);
 	echo $order->order();
 } else {
 	echo "tipe menu tidak ditemukan";
