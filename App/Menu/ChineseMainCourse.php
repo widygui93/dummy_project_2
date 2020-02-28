72ecffa0ab62749fe08a0baeafa8ac28b1a2310e
widygui93 <guilias.widy@gmail.com>
2020-02-28 21:27:15 +0700

    ubah isExtraSeafood jadi y di index.php agar bisa testing dan tambah property priceOfExtraSeafood dan totalPrice di chineseMainCourse.php

--- App/Menu/ChineseMainCourse.php
+++ App/Menu/ChineseMainCourse.php
@@ -1,12 +1,16 @@
 <?php namespace App\Menu;
 
 class ChineseMainCourse extends Menu{
-	private $isExtraSeafood;
+	private $isExtraSeafood,
+			$priceOfExtraSeafood,
+			$totalPrice;
 
 	public function __construct($namaMenu="Nama Menu",$hargaMenu=0,$isExtraSeafood="n",$type="chinese"){
 		parent::__construct($namaMenu, $hargaMenu, $type);
 
 		$this->isExtraSeafood = $isExtraSeafood;
+		$this->priceOfExtraSeafood = 0;
+		$this->totalPrice = $hargaMenu;
 	}
 
 	public function order(){
@@ -14,9 +18,11 @@ class ChineseMainCourse extends Menu{
 
 		if($this->isExtraSeafood == 'y'){
 			$str = " dengan extra seafood";
-			$this->hargaMenu += 10000;
+			// $this->hargaMenu += 10000;
+			$this->priceOfExtraSeafood = 10000;
+			$this->totalPrice = $this->hargaMenu + $this->priceOfExtraSeafood;
 		}
-		return "user order " . $this->namaMenu . $str . " type " . $this->typeMenu . " ( Rp." . $this->hargaMenu . " )";
+		return "user order " . $this->namaMenu . $str . " type " . $this->typeMenu . " ( Rp." . $this->totalPrice . " )";
 	}
 }
 
