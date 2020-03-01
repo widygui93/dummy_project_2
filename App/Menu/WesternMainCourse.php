1fbc392e6729dce0026d53d7f84765c0c1ebf6c2
widygui93 <guilias.widy@gmail.com>
2020-03-01 09:51:23 +0700

    pindahkan implementasi function cekUseAdditionalItem dan property total price dari WesternMainCourse.php ke Menu.php

--- App/Menu/WesternMainCourse.php
+++ App/Menu/WesternMainCourse.php
@@ -2,35 +2,21 @@
 
 class WesternMainCourse extends Menu{
 	private $isExtraHam,
-			$priceOfExtraHam,
-			$totalPrice;
+			$priceOfExtraHam;
 
 	public function __construct($namaMenu="Nama Menu",$hargaMenu=0,$isExtraHam="n",$type="western"){
 		parent::__construct($namaMenu, $hargaMenu, $type);
 
 		$this->isExtraHam = $isExtraHam;
 
-		$this->cekUseAdditionalItem();
+		$this->priceOfExtraHam = $this->cekUseAdditionalItem($this->isExtraHam);
 		
-	}
-
-	protected function cekUseAdditionalItem(){
-		if($this->isExtraHam == 'y'){
-			$this->priceOfExtraHam = 10000;
-			$this->totalPrice = $this->hargaMenu + $this->priceOfExtraHam;
-		} else {
-			$this->priceOfExtraHam = 0;
-			$this->totalPrice = $this->hargaMenu;
-		}
 	}
 
 	public function getPriceOfExtraHam(){
 		return $this->priceOfExtraHam;
 	}
 
-	public function getTotalPrice(){
-		return $this->totalPrice;
-	}
 
 
 	
