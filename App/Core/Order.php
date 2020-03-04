5950cd405639177800d2cca3a21b87f805fcf6c2
widygui93 <guilias.widy@gmail.com>
2020-03-04 22:06:41 +0700

    utk menentukan pake seafood/ham atau tidak diubah caranya pake conditional ternary

--- App/Core/Order.php
+++ App/Core/Order.php
@@ -35,7 +35,7 @@ if($type == "chinese"){
 		$menuChinese->getNamaMenu(),
 		$menuChinese->getHargaMenu(),
 		"user123",
-		"seafood",
+		($isExtraSeafood == y) ? "seafood" : "",
 		$menuChinese->getPriceOfExtraSeafood(),
 		$menuChinese->getTotalPrice()
 		)
@@ -50,7 +50,7 @@ if($type == "chinese"){
 		$menuWestern->getNamaMenu(),
 		$menuWestern->getHargaMenu(),
 		"user123",
-		"",
+		($isExtraHam == y) ? "ham" : "",
 		$menuWestern->getPriceOfExtraHam(),
 		$menuWestern->getTotalPrice()
 		)
