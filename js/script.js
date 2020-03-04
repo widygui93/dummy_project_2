fc939df2f81fc7958a895d11f8f3064b8d417e57
widygui93 <guilias.widy@gmail.com>
2020-03-04 21:18:40 +0700

    utk variable formatter,priceOfAdditionalItem,priceOfSelectedIem dan total di pindahkan di luar if agar tidak perlu declare 2 kali

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
 $(document).ready(function(){
 
 	$(":checkbox").change(function(){
+		const formatter = new Intl.NumberFormat('id-ID', {
+		  style: 'currency',
+		  currency: 'IDR',
+		  minimumFractionDigits: 0
+		});
+
+		let priceOfAddionalItem = parseInt($(this).val());
+		let priceOfSelectedItem = parseInt($(this).parent().next().next().next().val());
+		let total = priceOfSelectedItem + priceOfAddionalItem;
+
 		if(this.checked){
-			let priceOfAddionalItem = parseInt($(this).val());
-			let priceOfSelectedItem = parseInt($(this).parent().next().next().next().val());
-			let total = priceOfSelectedItem + priceOfAddionalItem;
-
-			const formatter = new Intl.NumberFormat('id-ID', {
-			  style: 'currency',
-			  currency: 'IDR',
-			  minimumFractionDigits: 0
-			});
-
 			$(this).parent().prev().children().text(formatter.format(total));
 		} else {
-			let priceOfSelectedItem = parseInt($(this).parent().next().next().next().val());
-			
-			const formatter = new Intl.NumberFormat('id-ID', {
-			  style: 'currency',
-			  currency: 'IDR',
-			  minimumFractionDigits: 0
-			});
-
 			$(this).parent().prev().children().text(formatter.format(priceOfSelectedItem));
 		}
 	});
