edcfefdbe734ea9cfebb006b5e8f6e402a315695
widygui93 <guilias.widy@gmail.com>
2020-03-08 11:21:43 +0700

    tambahkan style

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  </head>
  <body>
  	<div class="container-fluid">
- 		<div class="row" style="background-color:lightblue">
+ 		<div class="row">
  			<div class="col">
  				<h1>Your Brand</h1>
  			</div>
