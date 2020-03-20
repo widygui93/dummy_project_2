90e09953b434e5029b4bc54baef8f48e1da8a148
widygui93 <guilias.widy@gmail.com>
2020-03-20 11:03:20 +0700

    buat method updateID_transferFromTableOrder_menu dan method upload di file Db.php, panggil method updateID_TrnsferFromTableOrder_menu dari file Cart.php

--- App/Db/Cart.php
+++ App/Db/Cart.php
@@ -58,75 +58,12 @@ class Cart extends Db {
 				";
 		$resultInsert = $this->executeQuery($query);
 
-
-		$query = "SELECT id_transfer FROM transfer WHERE daftar_order_id LIKE '%133%'";
-
-		$resultSelect = $this->executeQuery($query);
-
-		$id_transfers = [];
-
-		while ( $row = mysqli_fetch_assoc($resultSelect[0]) ) {
-	        $id_transfers[] = $row;
-	    }
-
-	    $id_trf = 0;
-	    foreach ($id_transfers as $item) {
-			$id_trf = $item["id_transfer"];
-		}
-
-		foreach ($this->items as $item) {
-			$ord_ids = $item["order_id"];
-			$query = "UPDATE order_menu SET id_transfer = '$id_trf' WHERE order_id = '$ord_ids'";
-			$resultUpdate = $this->executeQuery($query);
-		}
+		$this->updateID_TransferFromTableOrder_Menu($this->items);
 
 		return $resultInsert[1];
 	}
 
-	private function upload(){
-		$namaFile = $_FILES['gambar']['name'];
-		$ukuranFile = $_FILES['gambar']['size'];
-		$error = $_FILES['gambar']['error'];
-		$tmpName = $_FILES['gambar']['tmp_name'];
 
-		// cek apakah tidak ada gambar yang diupload
-		if( $error === 4 ) {
-			echo "<script>
-					alert('pilih gambar terlebih dahulu!');
-				  </script>";
-			return false;
-		}
-
-		// cek apakah yang diupload adalah gambar
-		$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
-		$ekstensiGambar = explode('.', $namaFile);
-		$ekstensiGambar = strtolower(end($ekstensiGambar));
-		if( !in_array($ekstensiGambar, $ekstensiGambarValid) ) {
-			echo "<script>
-					alert('yang anda upload bukan gambar!');
-				  </script>";
-			return false;
-		}
-
-		// cek jika ukurannya terlalu besar
-		if( $ukuranFile > 1000000 ) {
-			echo "<script>
-					alert('ukuran gambar terlalu besar!');
-				  </script>";
-			return false;
-		}
-
-		// lolos pengecekan, gambar siap diupload
-		// generate nama gambar baru
-		$namaFileBaru = uniqid();
-		$namaFileBaru .= '.';
-		$namaFileBaru .= $ekstensiGambar;
-
-		move_uploaded_file($tmpName, 'img-transfer/' . $namaFileBaru);
-
-		return $namaFileBaru;
-
-	}
 
 }
 
--- App/Db/Db.php
+++ App/Db/Db.php
@@ -11,22 +11,6 @@ class Db {
 		$this->dbName = "dummy_project_2";
 	}
 
-	// protected function getServerName(){
-	// 	return $this->serverName;
-	// }
-
-	// protected function getUserName(){
-	// 	return $this->userName;
-	// }
-
-	// protected function getPassword(){
-	// 	return $this->password;
-	// }
-
-	// protected function getDbName(){
-	// 	return $this->dbName;
-	// }
-
 	private function connect(){
 		return mysqli_connect($this->serverName, $this->userName, $this->password, $this->dbName);
 	}
@@ -39,6 +23,70 @@ class Db {
 		$data[0] = $result;
 		$data[1] = $totalRows;
 		return $data;
+	}
+
+	protected function updateID_TransferFromTableOrder_Menu($items){
+		$order_ids = [];
+		foreach ($items as $item) {
+			array_push($order_ids,$item["order_id"]);
+		}
+
+		$query = "SELECT id_transfer FROM transfer WHERE daftar_order_id LIKE '%$order_ids[0]%'";
+
+		// $order_ids = [];
+
+		$resultSelect = $this->executeQuery($query);
+
+	    $id_transfer = mysqli_fetch_assoc($resultSelect[0]);
+
+		foreach ($items as $item) {
+			$query = "UPDATE order_menu SET id_transfer = '".$id_transfer["id_transfer"]."' WHERE order_id = '".$item["order_id"]."'";
+			$resultUpdate = $this->executeQuery($query);
+		}
+	}
+
+	protected function upload(){
+		$namaFile = $_FILES['gambar']['name'];
+		$ukuranFile = $_FILES['gambar']['size'];
+		$error = $_FILES['gambar']['error'];
+		$tmpName = $_FILES['gambar']['tmp_name'];
+
+		// cek apakah tidak ada gambar yang diupload
+		if( $error === 4 ) {
+			echo "<script>
+					alert('pilih gambar terlebih dahulu!');
+				  </script>";
+			return false;
+		}
+
+		// cek apakah yang diupload adalah gambar
+		$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+		$ekstensiGambar = explode('.', $namaFile);
+		$ekstensiGambar = strtolower(end($ekstensiGambar));
+		if( !in_array($ekstensiGambar, $ekstensiGambarValid) ) {
+			echo "<script>
+					alert('yang anda upload bukan gambar!');
+				  </script>";
+			return false;
+		}
+
+		// cek jika ukurannya terlalu besar
+		if( $ukuranFile > 1000000 ) {
+			echo "<script>
+					alert('ukuran gambar terlalu besar!');
+				  </script>";
+			return false;
+		}
+
+		// lolos pengecekan, gambar siap diupload
+		// generate nama gambar baru
+		$namaFileBaru = uniqid();
+		$namaFileBaru .= '.';
+		$namaFileBaru .= $ekstensiGambar;
+
+		move_uploaded_file($tmpName, 'img-transfer/' . $namaFileBaru);
+
+		return $namaFileBaru;
 	}
 
 	
