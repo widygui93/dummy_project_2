8dcf0e412e501c5cbbfcf801150fb592a45546e3
widygui93 <guilias.widy@gmail.com>
2020-03-24 14:06:31 +0700

    hapus comment di init.php

--- App/init.php
+++ App/init.php
@@ -1,28 +1,5 @@
 <?php 
 
-// require_once 'Menu/Menu.php';
-// require_once 'Db/Db.php';
-// require_once 'Menu/ChineseMainCourse.php';
-// require_once 'Menu/WesternMainCourse.php';
-// require_once 'Db/OrderMenu.php';
-
-
-
-
-// spl_autoload_register(function($class){
-// 	// $class = explode('\\', $class);
-// 	// $class = end($class);
-
-// 	require_once __DIR__ . '/Db/' .$class. '.php';
-// });
-
-// spl_autoload_register(function($class){
-// 	// $class = explode('\\', $class);
-// 	// $class = end($class);
-
-// 	require_once __DIR__ . '/Menu/' .$class. '.php';
-// });
-
 spl_autoload_register(function ($class) { 
 	$class = explode('\\', $class);
 	$class = end($class);
