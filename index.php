a8663884fb3301d3a1dc23008660e8f49b6c680d
widygui93 <guilias.widy@gmail.com>
2020-04-10 09:54:15 +0700

    di dindex.php hapus link all.min.cs dan di style.css hapus selector .row .col-2 karena sdh tidak ada lagi class itu di index.php

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ $cart = new Cart();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
-	<link rel="stylesheet" href="css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
  </head>
  <body>
