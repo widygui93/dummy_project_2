ec9337ea17225ec8b9a9b47d9862943e7d450f0b
widygui93 <guilias.widy@gmail.com>
2020-04-26 18:53:12 +0700

    di style.css tambahkan selector .header-item span dengan property margin-left 2px, dan di index.php hapus spasi di antara Cart dan span nya

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ $cart = new Cart();
                                     </li>
                                 </ul>
                                 <a href="App/Core/cart.php" class="header-item">
-                                    Cart <span class="badge badge-success"><?= $cart->getJumlahQuantity(); ?></span>
+                                    Cart<span class="badge badge-success"><?= $cart->getJumlahQuantity(); ?></span>
                                 </a>
                                 <!-- <a href="#" class="header-item"> -->
                                 <span id="header-price">Rp.4,000,000</span>
