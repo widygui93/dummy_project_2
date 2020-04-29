d6ed26d8c6712b816cd1df8a3fdc4b098bb7fbbe
widygui93 <guilias.widy@gmail.com>
2020-04-29 19:11:05 +0700

    di core/cart.php bungkus form payment ke dalam div class container-payment agar bisa di style di css nya, di file style.css tambahkan selector .container-payment dengan property border-radius, box-shadow, margin-bottom, margin-top, padding-bottom dan padding-left

--- App/Core/cart.php
+++ App/Core/cart.php
@@ -190,16 +190,18 @@ if( isset($_POST["submit"]) ) {
 		                        		</tr>
 		 							</table>
 	 							</div>
-	 							<form action="" method="post" enctype="multipart/form-data">
-								  <div class="form-group">
-								    <label for="norek">Please attach Transfer receipt below to account number <strong>12345678 (XXX)</strong>  Bank ABC. </label>
-								    <label>For delivery order to <strong>address street xxx no 12.</strong></label>
-								  </div>
-								  <div class="form-group">
-								  	<input type="file" name="gambar">
-								    <button type="submit" name="submit" class="btn btn-primary">Pay</button>
-								  </div>
-								</form>
+	 							<div class="container-payment">
+		 							<form action="" method="post" enctype="multipart/form-data">
+									  <div class="form-group">
+									    <label for="norek">Please attach Transfer receipt below to account number <strong>12345678 (XXX)</strong>  Bank ABC. </label>
+									    <label>For delivery order to <strong>address street xxx no 12.</strong></label>
+									  </div>
+									  <div class="form-group">
+									  	<input type="file" name="gambar">
+									    <button type="submit" name="submit" class="btn btn-primary">Pay</button>
+									  </div>
+									</form>
+								</div>
 							<?php endif; ?>
 
 							<?php if($showResult): ?>
