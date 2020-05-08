22a14f540f7e77722abcf627dd33d7e7d99299e1
widygui93 <guilias.widy@gmail.com>
2020-05-08 20:48:50 +0700

    di core/history.php pindahkan link yg memanggil style.css di bagian akhir(di bagian sebelum tag tutup body)

--- App/Core/history.php
+++ App/Core/history.php
@@ -42,8 +42,8 @@ $items = $history->getHistory();
 <head>
 	<meta charset="UTF-8">
 	<title>History</title>
-	
-    <link rel="stylesheet" type="text/css" href="../../css/style.css">
+
+    
 </head>
 <body>
 	<div class="modal fade" id="modalDetailTransaction" tabindex="-1" role="dialog" aria-labelledby="detailTransaction" aria-hidden="true">
@@ -282,5 +282,6 @@ $items = $history->getHistory();
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
 </body>
 </html>
