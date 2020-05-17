2c88dc5ffa1128928b754e205361547120676197
widygui93 <guilias.widy@gmail.com>
2020-05-17 20:27:33 +0700

    di core/history.php cuma tambahkan meta tag utk viewport agar bisa responsive utk device tablet dan mobile

--- App/Core/history.php
+++ App/Core/history.php
@@ -41,6 +41,7 @@ $items = $history->getHistory();
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>History</title>
 
     
