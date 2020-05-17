4f5c54b242f0be073265610fbc8989d9035abb47
widygui93 <guilias.widy@gmail.com>
2020-05-17 20:40:08 +0700

    di index.php tambahakn meta charset dan meta tag utk viewport di head

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $cart = new Cart();
  <!DOCTYPE html>
  <html>
  <head>
+ 	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
  	<title>Sunny Cafe</title>
 
  	<!-- CDN bootstrap 4 -->
