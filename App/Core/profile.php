b442e926d10954aae33ab6aba313b9e8b5e88e9c
widygui93 <guilias.widy@gmail.com>
2020-05-21 20:50:06 +0700

    di core/profile.php tambahkan cdn sweetalert di bagian head, saat modalEditProfileData di submit maka panggil method editProfileData di bagian body dan echo sweetalert nya

--- App/Core/profile.php
+++ App/Core/profile.php
@@ -14,22 +14,8 @@ $jlhQuantity = $cart->getJumlahQuantity();
 $userName = $profile->getUserName();
 $registerDate = $profile->getRegisterDate();
 
-if( isset($_POST["submitEditProfileData"]) ) {
-
-	if( $profile->editProfileData($_POST, $userName) > 0 ) {
-		echo "
-	        <script>
-	            alert('Edit data successfully');
-	        </script>
-	    ";
-	} 
-
-}
 
 
-$address = $profile->getAddress();
-$phoneNo = $profile->getPhoneNo();
-$email = $profile->getEmail();
 
 
 
@@ -47,6 +33,7 @@ $email = $profile->getEmail();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Profile</title>
+
 	<!-- CDN bootstrap 4 -->
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -54,8 +41,32 @@ $email = $profile->getEmail();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
     <link rel="stylesheet" type="text/css" href="../../css/style.css">
+	
+	<!-- CDN sweetalert -->
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	
 </head>
 <body>
+	<?php 
+		if( isset($_POST["submitEditProfileData"]) ) {
+
+			if( $profile->editProfileData($_POST, $userName) > 0 ) {
+				
+				echo "
+			        <script>
+
+			           swal('Success!', 'Edit data successfully', 'success');
+			        </script>
+			    ";
+			} 
+
+		}
+
+
+		$address = $profile->getAddress();
+		$phoneNo = $profile->getPhoneNo();
+		$email = $profile->getEmail();
+	?>
 	<!-- MODAL SECTION -->
 	<!-- MODAL EDIT PROFILE PIC -->
 	<div class="modal fade" id="modalEditProfilePic" tabindex="-1" role="dialog" aria-labelledby="editProfilePicture" aria-hidden="true">
@@ -355,6 +366,7 @@ $email = $profile->getEmail();
  	</div>
  	<script src="../../js/jquery-3.4.1.min.js"></script>
  	<script src="../../js/script.js"></script>
- 	
+
+
 </body>
 </html>
