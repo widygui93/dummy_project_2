a06914a45b385505db61c3cd26829be90f1b7b29
widygui93 <guilias.widy@gmail.com>
2020-05-22 21:34:43 +0700

    di login.php buat form login beserta class col bootstrap

--- App/Core/login.php
+++ App/Core/login.php
@@ -65,9 +65,29 @@
  			<div class="col-12">
  				<div class="container main-content">
  					<div class="row">
- 						<div class="col-sm">
- 							
- 							
+ 						<div class="col-12 mt-3 text-center text-uppercase">
+ 							<h2>Login</h2>
+ 						</div>
+ 					</div>
+ 					<div class="row">
+ 						<div class="col-lg-4 col-md-6 col-sm-8 mx-auto bg-white py-3 mb-4">
+ 							<div class="row">
+ 								<div class="col-12">
+ 									<form action="">
+ 										<div class="form-group">
+ 											<label for="username">Username</label>
+ 											<input type="text" name="username" id="username" class="form-control" required>
+ 										</div>
+ 										<div class="form-group">
+ 											<labe for="password">Password</label>
+ 											<input type="password" name="password" id="password" class="form-control" required></input>
+ 										</div>
+ 										<div class="form-group">
+ 											<button type="submit" class="btn btn-outline-dark">Login</button>
+ 										</div>
+ 									</form>
+ 								</div>
+ 							</div>
  						</div>
  					</div>
  				</div>
