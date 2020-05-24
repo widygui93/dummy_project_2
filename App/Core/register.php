8d7449a4db2bfe53144c4f7279aca03b18671fb8
widygui93 <guilias.widy@gmail.com>
2020-05-24 13:43:24 +0700

    di register.php di bagian input username tambahkan attribute minlength dan maxlength

--- App/Core/register.php
+++ App/Core/register.php
@@ -133,7 +133,7 @@ $profile = new Profile();
  									<form action="" method="post">
  										<div class="form-group">
  											<label for="username">Username</label>
- 											<input type="text" name="username" id="username" class="form-control" required>
+ 											<input type="text" name="username" id="username" minlength="6" maxlength="12" class="form-control" required>
  										</div>
  										<div class="form-group">
  											<label for="email">Email</label>
