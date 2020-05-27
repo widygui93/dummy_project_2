403641025906ed748b64861e15a801eadcdfd196
widygui93 <guilias.widy@gmail.com>
2020-05-27 21:55:47 +0700

    di DB/profile.php buat method login utk cek apakah ada username yg di input itu ada di DB klu ada cek apakah password nya sama jika sama return true klu g return false

--- App/Db/Profile.php
+++ App/Db/Profile.php
@@ -176,6 +176,26 @@ class Profile extends Db {
 
 	}
 
+	public function login($data) {
+		$username = strtolower(stripslashes($data["username"]));
+		$password = $this->escapeStr($data["password"]);
+
+		$query = "SELECT * FROM user WHERE username = '$username'";
+		$result = $this->executeQuery($query);
+
+		// cek username ada di db atau tidak
+		if( mysqli_num_rows($result[0]) === 1 ){
+			// cek password sama atau tidak
+			$row = mysqli_fetch_assoc($result[0]);
+			if( password_verify($password, $row["password"]) ){
+
+			    return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
 
 
 
