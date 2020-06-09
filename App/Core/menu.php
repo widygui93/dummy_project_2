48e9469c7daa59db6d5659f2184eac50fc6ed9b3
widygui93 <guilias.widy@gmail.com>
2020-06-09 20:10:39 +0700

    di core/menu.php hapus comment element div yg carousel slide w-100 lalu pindah class itu ke bagian div class col-12 yg menampung tipe menu dan menu nya

--- App/Core/menu.php
+++ App/Core/menu.php
@@ -168,35 +168,7 @@ $tipeMenu = $_GET["tipe"];
  			</div>
  			<div class="col-12">
  				<main class="row">
- 					<div class="col-12 px-0">
-                        <div id="slider" class="carousel slide w-100" data-ride="carousel">
-                            <!-- <ol class="carousel-indicators">
-                                <li data-target="#slider" data-slide-to="0" class="active"></li>
-                                <li data-target="#slider" data-slide-to="1"></li>
-                                <li data-target="#slider" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner" role="listbox">
-                                <div class="carousel-item active">
-                                    <img src="../Menu/Images/slider-1.jpg" class="slider-img">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="../Menu/Images/slider-2.jpg" class="slider-img">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="../Menu/Images/slider-3.jpg" class="slider-img">
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a> -->
-                        </div>
- 					</div>
- 					<div class="col-12">
+ 					<div class="col-12 carousel slide w-100">
  						<div class="row">
  							<div class="col-12 py-3">
  								<div class="row">
