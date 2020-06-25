98f53e9821e92abc8e20b49a984c1690f86d84d7
widygui93 <guilias.widy@gmail.com>
2020-06-25 20:29:11 +0700

    di admin/TipeMenu.php pake style yg di style.css, tambah nama kelas header,menu,main dan footer dgn -admin, gunakan menu seperti di menu.php, dan footer samakan dengan footer di menu.php

--- App/Admin/TipeMenu.php
+++ App/Admin/TipeMenu.php
@@ -15,29 +15,12 @@
 
     <!-- CDN sweetalert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <style type="text/css">
-    	.header { grid-area: header; }
-    	.menu { grid-area: menu; }
-    	.main { grid-area: main; }
-    	.footer { grid-area: footer; }
-
-		.grid-container {
-			display: grid;
-			grid-template-areas: 
-				'header header header header header header'
-				'menu main main main main main'
-				'footer footer footer footer footer footer';
-			grid-gap: 10px;
-		}
-
-    </style>
 </head>
 <body>
 	
-	<!-- <div class="navbar-custom topnav-navbar topnav-navbar-dark"> -->
 	<div class="container-fluid">
 		<div class="grid-container">
-			<div class="header">
+			<div class="header-admin">
 				<!-- Topbar Start -->
 				<div class="row min-vh-100">
 					<div class="col-12">
@@ -70,34 +53,36 @@
 				</div>
 				<!-- end Topbar -->
 			</div>
-			<div class="menu">
+			<div class="menu-admin">
 				<!-- start left sidebar -->
-				<div class="left-side-menu left-side-menu-detached mm-active">
-					<div class="leftbar-user">
-						<span>Menu Admin</span>
-					</div>
-					<ul class="metismenu side-nav mm-show">
-						<li class="side-nav-title side-nav-item">
-							<a href="../../index.php">Sunny Cafe App</a>
-						</li>
-						<li class="side-nav-item">
-							<a href="#"><span>Products</span></a>
-							<ul class="side-nav-second-level mm-collapse mm-show">
-								<li class="mm-active"><a href="TipeMenu.php" class="active">Tipe Menu</a></li>
-								<li><a href="menu.php">Menu</a></li>
-							</ul>
-						</li>
-						<li class="side-nav-item">
-							<a href="#"><span>Report</span></a>
-							<ul class="side-nav-second-level mm-collapse mm-show">
-								<li class="mm-active"><a href="#">Transaction Report</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light col-12">
+			        <button class="navbar-toggler d-lg-none border-0" type="button" data-toggle="collapse" data-target="#mainNav">
+			            <span class="navbar-toggler-icon"></span>
+			        </button>
+			        <div class="collapse navbar-collapse" id="mainNav">
+			            <ul class="nav flex-column navbar-nav mx-auto mt-2 mt-lg-0">
+			            	<li class="nav-item">
+			                    <strong>Menu Admin</strong>
+			                </li>
+			                <li class="nav-item">
+			                    <a class="nav-link" href="../../index.php">Sunny Cafe</a>
+			                </li>
+			                <li class="nav-item dropdown">
+			                    <a class="nav-link dropdown-toggle" href="#" id="products" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+			                    <div class="dropdown-menu" aria-labelledby="products">
+			                        <a class="dropdown-item" href="TipeMenu.php">Tipe Menu</a>
+			                        <a class="dropdown-item" href="menu.php">Menu</a>
+			                    </div>
+			                </li>
+			                <li class="nav-item">
+			                    <a class="nav-link" href="#">Transaction Report</a>
+			                </li>
+			            </ul>
+			        </div>
+			    </nav>
 				<!-- end left sidebar -->
 			</div>
-			<div class="main">
+			<div class="main-admin">
 				<!-- start content page -->
 				<div class="content-page">
 					<form action="" method="post">
@@ -134,102 +119,29 @@
 				</div>
 				<!-- end content page -->
 			</div>
-			<div class="footer">
+			<div class="footer-admin">
 				<div class="col-12 align-self-end">
 	 				<footer class="row">
-	 					<div class="col-12 bg-dark text-white pb-3 pt-5">
+	 					<div class="col-12 bg-dark text-white pb-3 pt-3 mb-3">
 	 						<div class="row">
-	 							<div class="col-lg-2 col-sm-4 text-center text-sm-left mb-sm-0 mb-3">
+	 							<div class="col-lg-6 col-sm-6 text-center text-sm-left">
 	                                <div class="row">
 	                                    <div class="col-12">
 	                                        <div class="footer-logo">
-	                                            <a href="index.php">E-Commerce</a>
+	                                            <img src="../Core/svg/wb_sunny-white-18dp.svg" alt="sunny">
+	                                            <strong>Sunny Cafe</strong>
+	                                            <img src="../Core/svg/wb_sunny-white-18dp.svg" alt="sunny">
 	                                        </div>
 	                                    </div>
-	                                    <div class="col-12">
-	                                        <address>
-	                                            221B Baker Street<br>
-	                                            London, England
-	                                        </address>
-	                                    </div>
 	                                </div>
 	                            </div>
-	                            <div class="col-lg-3 col-sm-8 text-center text-sm-left mb-sm-0 mb-3">
+	                            <div class="col-lg-6 col-sm-6 text-center text-sm-right">
 	                                <div class="row">
-	                                    <div class="col-12 text-uppercase">
-	                                        <h4>Who are we?</h4>
-	                                    </div>
-	                                    <div class="col-12 text-justify">
-	                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet vel ligula vel sodales. Aenean vel ullamcorper purus, ac pharetra arcu. Nam enim velit, ultricies eu orci nec, aliquam efficitur sem. Quisque in sapien a sem vestibulum volutpat at eu nibh. Suspendisse eget est metus. Maecenas mollis quis nisl ac malesuada. Donec gravida tortor massa, vitae semper leo sagittis a. Donec augue turpis, rutrum vitae augue ut, venenatis auctor nulla. Sed posuere at erat in consequat. Nunc congue justo ut ante sodales, bibendum blandit augue finibus.</p>
-	                                    </div>
-	                                </div>
-	                            </div>
-	                            <div class="col-lg-2 col-sm-3 col-5 ml-lg-auto ml-sm-0 ml-auto mb-sm-0 mb-3">
-	                                <div class="row">
-	                                    <div class="col-12 text-uppercase">
-	                                        <h4>Quick Links</h4>
-	                                    </div>
 	                                    <div class="col-12">
-	                                        <ul class="footer-nav">
-	                                            <li>
-	                                                <a href="#">Home</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Contact Us</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">About Us</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Privacy Policy</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Terms & Conditions</a>
-	                                            </li>
-	                                        </ul>
-	                                    </div>
-	                                </div>
-	                            </div>
-	                            <div class="col-lg-1 col-sm-2 col-4 mr-auto mb-sm-0 mb-3">
-	                                <div class="row">
-	                                    <div class="col-12 text-uppercase text-underline">
-	                                        <h4>Help</h4>
-	                                    </div>
-	                                    <div class="col-12">
-	                                        <ul class="footer-nav">
-	                                            <li>
-	                                                <a href="#">FAQs</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Shipping</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Returns</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Track Order</a>
-	                                            </li>
-	                                            <li>
-	                                                <a href="#">Report Fraud</a>
-	                                            </li>
-	                                        </ul>
-	                                    </div>
-	                                </div>
-	                            </div>
-	                            <div class="col-lg-3 col-sm-6 text-center text-sm-left">
-	                                <div class="row">
-	                                    <div class="col-12 text-uppercase">
-	                                        <h4>Newsletter</h4>
-	                                    </div>
-	                                    <div class="col-12">
-	                                        <form action="#">
-	                                            <div class="form-group">
-	                                                <input type="text" class="form-control" placeholder="Enter your email..." required>
-	                                            </div>
-	                                            <div class="form-group">
-	                                                <button class="btn btn-outline-light text-uppercase">Subscribe</button>
-	                                            </div>
-	                                        </form>
+	                                        <a href="https://github.com/widygui93"><img src="../Core/svg/github-white.svg" alt="github"></a>
+	                                        <a href="https://www.facebook.com"><img src="../Core/svg/facebook-white.svg" alt="facebook"></a>
+	                                        <a href="https://www.instagram.com/widygui/"><img src="../Core/svg/instagram-white.svg" alt="instagram"></a>
+	                                        <img src="../Core/svg/code-white.svg" alt="code" data-toggle="tooltip" data-placement="bottom" title="created by midymidy">
 	                                    </div>
 	                                </div>
 	                            </div>
@@ -240,8 +152,6 @@
 			</div>
 		</div>
 	</div>
-	
-	<!-- </div> -->
 	
 	
 </body>
