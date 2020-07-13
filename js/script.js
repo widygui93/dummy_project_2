ff07a1226a3c8bc1ed2e9b75bf435bf0cd9ee133
widygui93 <guilias.widy@gmail.com>
2020-07-13 19:38:49 +0700

    di script.js saat menu-btn di klik ganti dari toggleClass jadi kondisi jika ada class aktif maka remove class aktif dan add class inaktif dan sebaliknya, di style.css tambahkan selector inaktif yg property ny sama cuma beda di left aja dan animation-close nya dan di media query yg utk screen yg lebar paksa left jadi 0 di menu-admin agar saat class inaktif nyala maka di paksa left jadi 0 sehingga tetep muncul walau dalam screen lebar

--- js/script.js
+++ js/script.js
@@ -84,7 +84,14 @@ $(document).ready(function(){
 	});
 
 	$(".menu-btn").on('click',function(){
-		$(".menu-admin").toggleClass("aktif");
+		// $(".menu-admin").toggleClass("aktif");
+		if($(".menu-admin").hasClass('aktif')) {
+			$(".menu-admin").removeClass('aktif');
+			$(".menu-admin").addClass('inaktif');
+		} else {
+			$(".menu-admin").removeClass('inaktif');
+			$(".menu-admin").addClass('aktif');
+		} 
 		$(".menu-btn").toggleClass("open");
 	});
 
