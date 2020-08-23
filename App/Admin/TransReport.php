6b7fb8347a4b5f6e30ce91b7bb575bd66191815e
widygui93 <guilias.widy@gmail.com>
2020-08-23 14:39:51 +0700

    di file Admin/TransReport.php tambahkan use History, initial objek history, pindahkan CDN bootstrap dan link css ke akhir body,utk klik page tambahkan kondisi utk tidak klik detail,ketika klik detail panggil method getDetailHistory, munculkan modalDetailTransaction, isi get halaman dgn post halaman_aktif,panggil method searchTransBy, getTotalTrans, getTotalHalaman, dan getHalamanAktif, buat modalDetailTransaction, buat form utk detail dgn input display none utk id_transfer, halaman_aktif, from_trans_date, dan to_trans_date

--- App/Admin/TransReport.php
+++ App/Admin/TransReport.php
@@ -11,8 +11,10 @@ if ( !isset($_SESSION["admin"]) ) {
 }
 
 use App\Db\Report as Report;
+use App\Db\History as History;
 
 $report = new Report();
+$history = new History();
 
 $totalTrans = 0;
 ?>
@@ -24,13 +26,7 @@ $totalTrans = 0;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin-Transaction Report</title>
 
-	<!-- CDN bootstrap 4 -->
- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" type="text/css" href="../../css/style.css">
+	
 
     <!-- CDN sweetalert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
@@ -57,13 +53,75 @@ $totalTrans = 0;
 				}
 			}
 		}
-		if( !isset($_POST["search-trans"]) && isset($_GET["halaman"]) ){
+		if( !isset($_POST["detail"]) && !isset($_POST["search-trans"]) && isset($_GET["halaman"]) ){
 			$transReports = $report->searchTransBy( $_GET["fromtransdate"], $_GET["totransdate"]);
 		    $totalTrans = $report->getTotalTrans();
 		    $totalHalaman = $report->getTotalHalaman();
 			$halamanAktif = $report->getHalamanAktif();
 		}
+
+		if( isset($_POST["detail"]) ){
+		 	$details = $history->getDetailHistory($_POST["id_transfer"]);
+
+			echo '
+				<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+				<script>
+					$(window).load(function() {
+					    $("#modalDetailTransaction").modal("show");
+					});
+				</script>
+			';
+
+			$_GET["halaman"] = $_POST["halaman_aktif"];
+			$transReports = $report->searchTransBy( $_POST["from_trans_date"], $_POST["to_trans_date"]);
+		    $totalTrans = $report->getTotalTrans();
+		    $totalHalaman = $report->getTotalHalaman();
+			$halamanAktif = $report->getHalamanAktif();
+		}
 	?>
+	<div class="modal fade" id="modalDetailTransaction" tabindex="-1" role="dialog" aria-labelledby="detailTransaction" aria-hidden="true">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="detailTransaction">Detail Transaction</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					
+					<div class="table-responsive">
+						<table border="1" cellpadding="10" cellspacing="0" class="table table-dark table-hover">
+							<tr>
+								<th>No</th>
+								<th>Menu</th>
+								<th>Type Menu</th>
+								<th>Price</th>
+								<th>Quantity</th>
+								<th>Total Price</th>
+							</tr>
+							<?php $no=1; ?>
+							<?php foreach($details as $detail) : ?>
+								<tr>
+									<td><?= $no; ?></td>
+									<td><?= $detail["nama_menu"] ?></td>
+									<td><?= $detail["tipe_menu"] ?></td>
+									<td><?= number_format($detail["harga_menu"]) ?></td>
+									<td><?= $detail["quantity"] ?></td>
+									<td><?= number_format($detail["quantity"] * $detail["harga_menu"]) ?></td>
+								</tr>
+								<?php $no++; ?>
+							<?php endforeach; ?>
+						</table>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-secondary" type="button" data-dismiss="modal">OK</button>
+				</div>
+			</div>
+		</div>
+	</div>
 	<!-- <div class="container-fluid"> -->
 		<div class="grid-container">
 			<header class="header-admin">
@@ -206,7 +264,15 @@ $totalTrans = 0;
 								<td><?= $transReport['tgl_transfer']; ?></td>
 								<td><?= $transReport['alamat_order']; ?></td>
 								<td><a href="">View receipt</a></td>
-								<td><a href="">View detail</a></td>
+								<td>
+									<form action="" method="post">
+										<input type="text" style="display: none;" name="id_transfer" value=<?= $transReport["id_transfer"]; ?> >
+										<input type="text" style="display: none;" name="halaman_aktif" value=<?= $halamanAktif; ?> >
+										<input type="text" style="display: none;" name="from_trans_date" value=<?= $report->getFromTransDate(); ?> >
+										<input type="text" style="display: none;" name="to_trans_date" value=<?= $report->getToTransDate(); ?> >
+										<button type="submit" name="detail" class="btn btn-success">View Detail</button>
+									</form>
+								</td>
 							</tr>
 							<?php $no++; ?>
 							<?php endforeach; ?>
@@ -299,6 +365,13 @@ $totalTrans = 0;
 	<!-- </div> -->
 	<script src="../../js/jquery-3.4.1.min.js"></script>
 	<script src="../../js/script.js"></script>
+	<!-- CDN bootstrap 4 -->
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
 	
 	
 </body>
